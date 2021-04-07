9b59856ff05f2abd64d7570c00ccd0f437fdb94f
Nubi <nubi@raonhive.win>
2021-04-07 14:21:42 +0900

    입력받은 값 출력

--- test.js
+++ test.js
@@ -1,3 +1,10 @@
-let myName = "Nubi";
+let lastPage = "https://ko.javascript.info/alert-prompt-confirm";
 
-console.log( `My name is ${myName}!` );
+let readline = require('readline');
+let r = readline.createInterface({
+    input:process.stdin,
+    output:process.stdout
+});
+r.question("나이를 입력해주세요.",function(age) {
+    console.log( `당신의 나이는 ${age}살 입니다!` );
+});
