40ac3f8e55066efd0cc0bcd1abce6e1bd3d76923
Nubi <nubi@raonhive.win>
2021-04-08 16:04:38 +0900

    시간경과(변수를 문자열로 인식해서 수정해야됨)

--- test.js
+++ test.js
@@ -5,6 +5,15 @@ let r = readline.createInterface({
     input:process.stdin,
     output:process.stdout
 });
+
 r.question("나이를 입력해주세요.",function(age) {
     console.log( `당신의 나이는 ${age}살 입니다!` );
+
+    r.question("시간 경과",function(time) {
+        console.log( `${time}년 후` );
+
+        console.log( `${time}년 후에는 ${age + time}살이 됩니다!` );
+
+    });
+
 });
